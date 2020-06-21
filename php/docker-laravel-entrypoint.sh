f286df29a3c7d05049f6e430d232e95565710cb8
xunliqiang <lq_xun@163.com>
2020-06-22 01:26:16 +0800

    修改php入口文件逻辑，添加默认phpinfo.php文件

--- php/docker-laravel-entrypoint.sh
+++ php/docker-laravel-entrypoint.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
-if [[ ${PROJECT_CREATE}="true" && "`ls -A ${PROJECT_NAME}`" = "" ]]; then
-    composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
-    composer create-project --prefer-dist laravel/laravel ${PROJECT_NAME}
+PHPINFO_EXISTS=`ls -A | grep phpinfo.php`
+FILE_COUNT=`ls -A | wc -l`
 
+if [[ ${PROJECT_CREATE}="true" ]]; then
+
+    if [[ ${FILE_COUNT} = 0 || (${FILE_COUNT} = 1 && ${PHPINFO_EXISTS}) ]]; then
+        `rm -rf *`
+        composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
+        composer create-project --prefer-dist laravel/laravel ${PROJECT_NAME}
+        echo "<?php phpinfo();" > phpinfo.php
+    fi
+fi
+
+
+PHPINFO_EXISTS=`ls -A | grep phpinfo.php`
+
+if [[ -z ${PHPINFO_EXISTS} ]]; then
     echo "<?php phpinfo();" > phpinfo.php
 fi
 
