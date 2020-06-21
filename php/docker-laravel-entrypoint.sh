7b6490dd3ff6374485587513b0c9b1a634d835dc
xunliqiang <lq_xun@163.com>
2020-06-21 18:57:14 +0800

    删除默认的phpinfo文件，在脚本中生成

--- php/docker-laravel-entrypoint.sh
+++ php/docker-laravel-entrypoint.sh
@@ -3,6 +3,8 @@
 if [[ ${PROJECT_CREATE}="true" && "`ls -A ${PROJECT_NAME}`" = "" ]]; then
     composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
     composer create-project --prefer-dist laravel/laravel ${PROJECT_NAME}
+
+    echo "<?php phpinfo();" > phpinfo.php
 fi
 
 exec "php-fpm"
