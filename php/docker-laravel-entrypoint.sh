04d57deb5e6c4ddf033cb3a69f7152a3d1fbca3e
xunliqiang <lq_xun@163.com>
2020-06-22 01:56:40 +0800

    修复php入口文件

--- php/docker-laravel-entrypoint.sh
+++ php/docker-laravel-entrypoint.sh
@@ -5,7 +5,7 @@ FILE_COUNT=`ls -A | wc -l`
 
 if [[ ${PROJECT_CREATE}="true" ]]; then
 
-    if [[ ${FILE_COUNT} = 0 || (${FILE_COUNT} = 1 && ${PHPINFO_EXISTS}) ]]; then
+    if [[ ${FILE_COUNT} = 0 || ${FILE_COUNT} = 1 && ${PHPINFO_EXISTS} ]]; then
         `rm -rf *`
         composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
         composer create-project --prefer-dist laravel/laravel ${PROJECT_NAME}
